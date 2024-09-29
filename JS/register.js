abf634c0d81195286bd438d840e25d81bff6ce6f
Daniel Andres Tobon Comba <toboncombadanielandres@gmail.com>
2024-09-28 19:30:36 -0500

    ultimos arreglos

--- JS/register.js
+++ JS/register.js
@@ -1,28 +1,27 @@
 // register.js
+// Función para regresar a la página de login
+function backToLogin() {
+    window.location.href = '/HTML/loggin.html';
+}
 
+// Registro de usuario
 const registerForm = document.getElementById('registerForm');
-
-registerForm.addEventListener('submit', function(event) {
+registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // Obtener valores ingresados en el formulario de registro
     const newUsername = document.getElementById('newUsername').value;
     const newPassword = document.getElementById('newPassword').value;
 
-    // Obtener los usuarios almacenados desde localStorage
-    let users = JSON.parse(localStorage.getItem('users'));
-
-    // Agregar nuevo usuario al diccionario
-    users[newUsername] = newPassword;
-
-    // Guardar nuevamente en localStorage
-    localStorage.setItem('users', JSON.stringify(users));
-
-    // Iniciar la sesión del nuevo usuario
-    localStorage.setItem('currentUser', newUsername);
-
-    alert('Usuario creado exitosamente');
-
-    // Redirigir a la página de bienvenida
-    window.location.href = '/HTML/bienvenida.html';
+    // Almacenar el nuevo usuario en localStorage
+    const users = JSON.parse(localStorage.getItem('users')) || {};
+    if (users[newUsername]) {
+        alert('El usuario ya existe. Prueba con otro nombre.');
+    } else {
+        users[newUsername] = newPassword;
+        localStorage.setItem('users', JSON.stringify(users));
+        alert('Usuario creado con éxito');
+        window.location.href = '/HTML/loggin.html'; // Redirigir a login tras registrar
+    }
 });
+
+
